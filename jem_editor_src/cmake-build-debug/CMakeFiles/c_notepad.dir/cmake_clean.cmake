f50681f3ea623d0d9afb469ffa86cd917c6cc6ff
Jose Maldonado <emanueljose11@hotmail.com>
2020-07-24 13:16:14 -0700

    *** SEARCH FUNCTION ADDED, CMAKE FILES ADDED *** You can now search the document. The search is iterative, everytime a new char is pressed/deleted it automatically tries to find a match of those certain characters in the document. If there are multiple sets of successfull matches you can use the arrow keys to go to the next match.
    Cmake files have also been added. An unnamed friend essentially
    bullied me into adding appropriate cmake files to make sure all
    libraries and any additional external dependencies are automatically
    taken care of.
    Signed-off-by: Jose Maldonado <emanueljose11@hotmail.com>

---
+++ jem_editor_src/cmake-build-debug/CMakeFiles/c_notepad.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/c_notepad.dir/main.c.o"
+  "c_notepad.exe"
+  "c_notepad.pdb"
+  "libc_notepad.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/c_notepad.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
